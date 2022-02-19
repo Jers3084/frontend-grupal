33a749ed55a615791d168958c4f00c0305a9cbec
JESUS EUGENIO RICARDEZ <jericrdez.c03@ucamp.utel.edu.mx>
2022-02-19 12:26:02 -0600

    commit
    Se agregaron rutas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import { Registro } from "./componets/Registro";
+import { Login } from "./componets/Login";
+import { Listado } from "./componets/Listado";
+import { Nav } from "./componets/Nav";
+
 
 function App() {
   return (
     <div className="App">
-      
-
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route path="/" exact></Route>
+          <Route path="/registro">
+            <Registro />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/listado">
+            <Listado />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
