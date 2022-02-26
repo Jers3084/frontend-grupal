f0177ce59376087f1f3fa492de88420178a4ec4e
Jers3084 <jers1968@gmail.com>
2022-02-25 18:05:43 -0600

    update

--- src/componets/Listado.jsx
+++ src/componets/Listado.jsx
@@ -2,25 +2,20 @@ import React, { useEffect, useState } from "react";
 
 export const Listado = () => {
   const [listado, setListado] = useState([]);
-  const [correo, setCorreo] = useState("");
-  const [username, setUsername] = useState("");
-  const [token, setToken] = useState("");
 
   const obtenerUsuarios = async () => {
     try {
-      const usuarios = async () => {
-        const response = await fetch("http://Localhost:3001/api/usuarios", {
-          method: "GET",
-          headers: {
-            authorization:
-              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMTQ2Y2Q4OGNhZDQ5YzUyYmRjYmUxNCIsInVzZXJuYW1lIjoiZGllZ29tIiwiaWF0IjoxNjQ1NTg0NDQ3LCJleHAiOjE2NDU1ODgwNDd9.L52f3qrwWNpzWHQI2DYIDjS-5PXBU8beZ9GKv9bOdZQ",
-          },
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            setListado(data.data);
-          });
-      };
+      const response = await fetch("35.192.83.171:3500/api/usuarios", {
+        method: "GET",
+        headers: {
+          authorization:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMTQ2Y2Q4OGNhZDQ5YzUyYmRjYmUxNCIsInVzZXJuYW1lIjoiZGllZ29tIiwiaWF0IjoxNjQ1NTg0NDQ3LCJleHAiOjE2NDU1ODgwNDd9.L52f3qrwWNpzWHQI2DYIDjS-5PXBU8beZ9GKv9bOdZQ",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          return setListado(data.data);
+        });
     } catch (e) {
       console.log("hubo un error");
       console.log(e);
@@ -35,7 +30,7 @@ export const Listado = () => {
     <div>
       <h1>Listado</h1>
       <ul>
-        {Listado.map((x) => {
+        {listado.map((x) => {
           <li key={x.id}>{x.username}</li>;
         })}
       </ul>
